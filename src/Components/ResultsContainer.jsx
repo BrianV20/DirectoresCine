e1b9d8d72a55e81149775bdd66367879bf81d244
Brian <brianezequielv2004@gmail.com>
2024-06-11 18:20:27 -0300

    cambio de rumbo. Ahora solo voy a poder elegir de entre ciertos directores, porque consumir esta api para realizar búsquedas es demasiado complejo y tedioso

--- src/Components/ResultsContainer.jsx
+++ src/Components/ResultsContainer.jsx
@@ -1,31 +1,101 @@
 import { useState, useEffect, useContext } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { DirectorContext, SearchContext } from "../Contexts";
+import { API_URL, options } from "../constants";
 
 export default function ResultsContainer() {
     const location = useLocation();
-    const { searchResults, setSearchResults } = useContext(SearchContext);
-    const [selectedType, setSelectedType] = useState('Directors');
+    const { searchResults, setSearchResults } = useContext(SearchContext); //los resultados que vienen del contexto.
+    const [selectedType, setSelectedType] = useState('person'); //el tipo de resultado seleccionado: person(Directores) y movie(Peliculas).
+    const [actualPage, setActualPage] = useState(1); //la página actual (que es modificada mediante los numeros de las págionas abajo de todo).
+    const [actualResults, setActualResults] = useState({}); //los resultados que debo mostrar (van a estar determinados por actualPage y selectedType).
+    // const [actualSearch, setActualSearch] = useState('');
     // const [selectedResults]
 
     const handleClick = (buttonType) => {
-        if(selectedType != buttonType){
-            selectedType(buttonType);
+        if (selectedType != buttonType) {
+            setSelectedType(buttonType);
         };
     };
 
-    // useEffect
+    const getAllDirectorsFromResults = async (array) => {
+        let temp = [];
+
+        for (let i = 0; i < array.results.length; i++) {
+            if (array.results[i].known_for_department == 'Directing') {
+                temp.push(array.results[i]);
+            };
+        };
+
+        return temp;
+    };
+
+    useEffect(() => {
+        const fetchThings = async () => {
+            const response = await fetch(`${API_URL}/search/${selectedType}?query=${location.pathname.split('/:')[1]}&include_adult=false&language=en-US&page=${actualPage}`, options);
+            const data = await response.json();
+            if (selectedType == 'person') {
+                let temp = await getAllDirectorsFromResults(data);
+                setActualResults(temp);
+            }
+            else {
+                setActualResults(data);
+            }
+            console.log(actualResults);
+            // console.log(`${API_URL}/search/${selectedType}?query=${location.pathname.split('/:')[1]}&include_adult=false&language=en-US&page=${actualPage}`);
+            // console.log("Data ⏬");
+            // console.log(data);
+            // setActualPage(data);
+            // // console.log(location.pathname.split('/:')[1]);
+            // console.log("Actual page: " + actualPage);
+            // console.log(searchResults);
+        };
+
+        fetchThings();
+    }, [actualPage, searchResults, selectedType]);
+
+    useEffect(() => {}, []);
 
     return (
         <>
-        {/* <p>Este es el results container</p> */}
-        {console.log(searchResults)}
-        <div className="bg-red-200 mx-2 flex gap-x-5">
-            <button onClick={() => handleClick('Directors')} className="rounded-xl text-xl text-center border-2 border-black py-1 px-3 hover:bg-green-200 hover:cursor-pointer">Directors</button>
-            <button onClick={() => handleClick('Movies')} className="rounded-xl text-xl text-center border-2 border-black py-1 px-3 hover:bg-green-200 hover:cursor-pointer">Movies</button>
-        </div>
+            {/* <p>Este es el results container</p> */}
+            {/* {console.log(typeof searchResults)} */}
+            {/* {console.log(searchResults)} */}
+            <div className="bg-red-200 mx-2">
+                <div className="flex gap-x-5">
+                    <button onClick={() => handleClick('person')} className="rounded-xl text-xl text-center border-2 border-black py-1 px-3 hover:bg-green-200 hover:cursor-pointer">Directors</button>
+                    <button onClick={() => handleClick('movie')} className="rounded-xl text-xl text-center border-2 border-black py-1 px-3 hover:bg-green-200 hover:cursor-pointer">Movies</button>
+                </div>
 
-        <div>ACA MOSTRARIA LOS RESULTADOS, POR DEFECTO MOSTRARÉ LOS DIRECTORES PERO SI SE HACE CLICK EN MOVIES SE MOSTRARÁN LAS MOVIES.</div>
+                <div className="bg-red-400 mx-2 flex flex-col">
+                    {actualResults.results != undefined && actualResults.results.map((result, i) => {
+                        {
+                            selectedType == 'person'(
+                                <div key={result.name + i} className="bg-blue-300 border-2 border-black">
+                                    <p>{result.name}</p>
+                                </div>
+                            )
+                        }
+                    })}
+                </div>
+                {/* //aca falte hacer la logica para mostrar de forma dinámica los numeros del paginado */}
+                <div>
+                    <p onClick={() => setActualPage(1)}>1</p>
+                    <p onClick={() => setActualPage(2)}>2</p>
+                    <p onClick={() => setActualPage(3)}>3</p>
+                </div>
+                {/* <div>
+                {selectedType == 'Directors' ? (
+                    <div className="bg-blue-400 flex flex-col">
+                        {searchResults.persons.map((p, i) => {
+                            <div key={p.}></div>
+                        })}
+                    </div>
+                ) : (
+                    <p>Movies</p>
+                )}
+            </div> */}
+            </div>
         </>
     )
 };
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -5,96 +5,48 @@ import { SearchContext } from "../Contexts";
 
 export default function SearchBar() {
     const [searchText, setSearchText] = useState('');
-    const [results, setResults] = useState({});
+    // const [results, setResults] = useState({});
     const navigate = useNavigate();
     const { searchResults, setSearchResults } = useContext(SearchContext);
-    const search = async () => {
-        if(searchText != ''){
-            let personResults = await searchSomething("person");
-            let movieResults = await searchSomething("movie");
-
-            // console.log("PERSON RESULTS ⏬");
-            // console.log(personResults);
-
-            let directorsFromResults = await filterDirectors(personResults);
-            let moviesFromResults = await getAllEntitiesFromSearchResults('movie', movieResults);
-            // console.log(moviesFromResults);
-
-            let allResults = {
-                persons: directorsFromResults,
-                movies: moviesFromResults
-            };
-            // console.log("\n\nMOVIES FROM RESULTS ⏬");
-            // console.log(moviesFromResults);
-            setResults(allResults);
-            // console.log(allResults);
-            setSearchResults(allResults);
-            navigate(`/search/:${searchText}`);
-        }
+    
+    const searchHandler = async (e) => {
+        let lowercase = e.target.value.toLowerCase();
+        await setSearchText(lowercase);
     };
 
-    const filterDirectors = async (array) => {
-        // console.log(`Page: ${array.page}.\nTotal pages: ${array.total_pages}.\nTotal results: ${array.total_results}.`);
-        let tempArray = await getAllEntitiesFromSearchResults('person', array); //este seria el array de todas las personas que vinieron como resultado.
-        let filteredArray = tempArray.filter(person => person.known_for_department == 'Directing');
-        // console.log("ESTOS SON LOS DIRECTORES ⏬");
-        // console.log(filteredArray);
-        return filteredArray;
+    //función que se ejecuta al hacer click en la lupa
+    const searchTheText = async () => {
+        let movieResults = await searchSomething('movie');
+        let personResults = await searchSomething('person');
+
+        let allResults = {
+            people: personResults,
+            movies: movieResults
+        };
+
+        // console.log(allResults);
+        // setResults(allResults);
+        setSearchResults(allResults);
+        navigate(`/search/:${searchText}`);
     };
-
-    const getAllEntitiesFromSearchResults = async (entity, array) => {
-        let temp = [];
-        for(let i = 1; i <= array.total_pages; i++){
-            if(i > 1){
-                let response = await fetch(`${API_URL}/search/${entity}?query=${searchText}&include_adult=false&language=en-US&page=${i}`, options);
-                let data = await response.json();
-
-                for(let c = 0; c < data.results.length; c++){
-                    // console.log(`PAGINA NRO ${i}. NRO DE RESULTADOS DE ESTA PAGINA: ${data.results.length}`);
-                    temp.push(data.results[c]);
-                }
-            }
-            else {
-                for(let j = 0; j < array.results.length; j++){
-                    // console.log(`PAGINA NRO ${i}. NRO DE RESULTADOS DE ESTA PAGINA: ${array.results.length}`);
-                    temp.push(array.results[j]);
-                }
-            }
-        }
-
-        // console.log("ESTE SERIA EL RESULTADO DESPUES DE PASAR POR LA FUNCION getAllEntitiesFromSearchResults ⏬");
-        // console.log(temp);
-        return temp;
-    };
-
-    // const filterMovies = async (array) => {
-    //     console.log(`Page: ${array.page}.\nTotal pages: ${array.total_pages}.\nTotal results: ${array.total_results}.`);
-
-    // };
 
     const searchSomething = async (entity) => {
         let response = await fetch(`${API_URL}/search/${entity}?query=${searchText}&include_adult=false&language=en-US`, options);
         let data = await response.json();
-        // console.log(data.results);
         return data;
     };
-
-    const searchHandler = async (e) => {
-        let lowercase = e.target.value.toLowerCase();
-        setSearchText(lowercase);
-    };
-
-    // useEffect(() => {
         
-    // }, []);
-
-    return (
+        // useEffect(() => {
+        
+            // }, []);
+            
+            return (
         <>
         <div className="bg-green-300 flex justify-end py-3 px-2">
             {/* <div class="flex items-center justify-center p-5">
                 <div class="rounded-lg bg-gray-200 p-5"> */}
                     <div className="flex">
-                        <div className="flex w-10 items-center justify-center rounded-tl-lg rounded-bl-lg border-r border-gray-200 bg-white p-5 hover:cursor-pointer" onClick={() => search()}>
+                        <div className="flex w-10 items-center justify-center rounded-tl-lg rounded-bl-lg border-r border-gray-200 bg-white p-5 hover:cursor-pointer" onClick={() => searchTheText()}>
                             <svg viewBox="0 0 20 20" aria-hidden="true" className="pointer-events-none absolute w-5 fill-gray-500 transition">
                                 <path d="M16.72 17.78a.75.75 0 1 0 1.06-1.06l-1.06 1.06ZM9 14.5A5.5 5.5 0 0 1 3.5 9H2a7 7 0 0 0 7 7v-1.5ZM3.5 9A5.5 5.5 0 0 1 9 3.5V2a7 7 0 0 0-7 7h1.5ZM9 3.5A5.5 5.5 0 0 1 14.5 9H16a7 7 0 0 0-7-7v1.5Zm3.89 10.45 3.83 3.83 1.06-1.06-3.83-3.83-1.06 1.06ZM14.5 9a5.48 5.48 0 0 1-1.61 3.89l1.06 1.06A6.98 6.98 0 0 0 16 9h-1.5Zm-1.61 3.89A5.48 5.48 0 0 1 9 14.5V16a6.98 6.98 0 0 0 4.95-2.05l-1.06-1.06Z"></path>
                             </svg>
@@ -107,4 +59,117 @@ export default function SearchBar() {
         </div>
         </>
     )
-};
+    };
+
+
+
+
+
+
+
+
+    
+        // const search = async () => {
+        //     if(searchText != ''){
+        //         let personResults = await searchSomething("person");
+        //         let movieResults = await searchSomething("movie");
+    
+        //         console.log("aca antes de filtrar los directores");
+    
+        //         // console.log("PERSON RESULTS ⏬");
+        //         // console.log(personResults);
+    
+        //         let directorsFromResults = await filterDirectors(personResults);
+        //         let moviesFromResults = await getAllEntitiesFromSearchResults('movie', movieResults);
+        //         // console.log(moviesFromResults);
+    
+        //         let allResults = {
+        //             persons: directorsFromResults,
+        //             movies: moviesFromResults
+        //         };
+        //         // console.log("\n\nMOVIES FROM RESULTS ⏬");
+        //         // console.log(moviesFromResults);
+        //         setResults(allResults);
+        //         // console.log(allResults);
+        //         setSearchResults(allResults);
+        //         console.log("ACA YA SE SETEARON LOS RESULTADOS EN EL ESTADO");
+        //         navigate(`/search/:${searchText}`);
+        //     }
+        // };
+    
+        // const filterDirectors = async (array) => {
+        //     // console.log(`Page: ${array.page}.\nTotal pages: ${array.total_pages}.\nTotal results: ${array.total_results}.`);
+        //     let tempArray = await getAllEntitiesFromSearchResults('person', array); //este seria el array de todas las personas que vinieron como resultado.
+        //     let filteredArray = tempArray.filter(person => person.known_for_department == 'Directing');
+        //     // console.log("ESTOS SON LOS DIRECTORES ⏬");
+        //     // console.log(filteredArray);
+        //     return filteredArray;
+        // };
+    
+    
+        // const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+    
+        // const getAllEntitiesFromSearchResults = async (entity, array) => {
+        //     //       MI LOCIGA
+        //     // let temp = [];
+        //     // for(let i = 1; i <= array.total_pages; i++){
+        //     //     if(i > 1){
+        //     //         let response = await fetch(`${API_URL}/search/${entity}?query=${searchText}&include_adult=false&language=en-US&page=${i}`, options);
+        //     //         let data = await response.json();
+    
+        //     //         for(let c = 0; c < data.results.length; c++){
+        //     //             console.log(`PAGINA NRO ${i}. NRO DE RESULTADOS DE ESTA PAGINA: ${data.results.length}`);
+        //     //             temp.push(data.results[c]);
+        //     //         }
+        //     //     }
+        //     //     else {
+        //     //         for(let j = 0; j < array.results.length; j++){
+        //     //             console.log(`PAGINA NRO ${i}. NRO DE RESULTADOS DE ESTA PAGINA: ${array.results.length}`);
+        //     //             temp.push(array.results[j]);
+        //     //         }
+        //     //     }
+        //     // }
+    
+        //     // let promises = [];
+        //     // for(let i = 0; i<= array.total_pages; i++){
+        //     //     let promise = fetch(`${API_URL}/search/${entity}?query=${searchText}&include_adult=false&language=en-US&page=${i}`, options)
+        //     //         .then(response => response.json())
+        //     //         .then(data => data.results);
+        //     //     promises.push(promise);
+        //     // };
+    
+        //     // let results = await Promise.all(promises);
+        //     // let temp = [].concat(...results);
+        //     // console.log("ESTE ES EL ARRAY QUE DEVUELVE LA NUEVA FUNCION ");
+        //     // console.log(temp);
+    
+    
+        //     let temp = [];
+        //     const delayBetweenRequests = 200; // 200ms delay between requests
+        
+        //     for(let i = 0; i <= array.total_pages; i++){
+        //         await delay(delayBetweenRequests);
+        //         let response = await fetch(`${API_URL}/search/${entity}?query=${searchText}&include_adult=false&language=en-US&page=${i}`, options);
+        //         let data = await response.json();
+        //         temp.push(...data.results);
+        //     }
+        
+        //     console.log("ESTE ES EL ARRAY QUE DEVUELVE LA NUEVA FUNCION ");
+        //     console.log(temp);
+    
+        //     // console.log("ESTE SERIA EL RESULTADO DESPUES DE PASAR POR LA FUNCION getAllEntitiesFromSearchResults ⏬");
+        //     // console.log(temp);
+        //     return temp;
+        // };
+    
+        // // const filterMovies = async (array) => {
+        // //     console.log(`Page: ${array.page}.\nTotal pages: ${array.total_pages}.\nTotal results: ${array.total_results}.`);
+    
+        // // };
+    
+        // const searchSomething = async (entity) => {
+        //     let response = await fetch(`${API_URL}/search/${entity}?query=${searchText}&include_adult=false&language=en-US`, options);
+        //     let data = await response.json();
+        //     // console.log(data.results);
+        //     return data;
+        // };
